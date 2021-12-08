2f61c1a3fc1b065cd74007f6b2a34eab6d82cf3a
Aaliyah Demitro <aaliyahdemitro900@icloud.com>
2021-12-08 12:23:37 -0500

    Initial Commit

---
+++ Alist/AlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlistApp.swift
+//  Alist
+//
+//  Created by Aaliyah Demitro on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct AlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
